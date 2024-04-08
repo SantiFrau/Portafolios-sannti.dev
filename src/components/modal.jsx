b816b6e122bd2e921fab77be2698fb3967600556
Santi <santiagofrau3@gmail.com>
2024-04-08 09:48:02 -0300

    add links poryectos

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -47,11 +47,11 @@ export default function Modal_info({setProyecto_info,proyecto_info}) {
     return (
         <div onClick={()=>{setProyecto_info(false);setImagen(1)}}  className={`z-20  w-full h-full fixed top-0 left-0 bg-black bg-opacity-60 flex flex-col items-center justify-center`} >
        
-          <div className="animacion2 relative bg-zinc-900 w-full sm:w-2/3 h-max flex flex-col gap-5 p-5 py-10 justify-center items-center rounded-lg">
+          <div onClick={(event)=>{event.stopPropagation()}}  className="animacion2 relative bg-zinc-900 w-full sm:w-2/3 h-max flex flex-col gap-5 p-5 py-10 justify-center items-center rounded-lg">
           <button className="text-white hover:text-zinc-400 text-2xl absolute top-0 right-0 p-3 px-5" onClick={()=>{setProyecto_info(false);setImagen(1)}}>x</button>
             <div className="flex flex-row gap-3 text-white items-center justify-center">
 
-            <div className="w-max h-full flex items-center justify-center hover:text-zinc-400 cursor-pointer px-3" onClick={(event)=>{event.stopPropagation(); previous(proyecto_info.imagenes)}} >
+            <div className="w-max h-full flex items-center justify-center hover:text-zinc-400 cursor-pointer sm:px-3 px-1" onClick={()=>{ previous(proyecto_info.imagenes)}} >
             <ArrowBackIosIcon  style={{fontSize:36}}></ArrowBackIosIcon>
             </div>
             
@@ -59,7 +59,7 @@ export default function Modal_info({setProyecto_info,proyecto_info}) {
             <Img className={`${animacion.movimiento} z-10 w-2/3 `} src={`/Portafolios-sannti.dev/proyectos/${proyecto_info.id}/${imagen}.webp`}  />
             <Img className={`w-2/3 ${animacion.movimiento} ${animacion.desaparecer}  absolute z-0 `} src={`/Portafolios-sannti.dev/proyectos/${proyecto_info.id}/${prevSource}.webp`}
              />
-             <div className="w-max h-full flex items-center justify-center hover:text-zinc-400 cursor-pointer px-3" onClick={(event)=>{event.stopPropagation(); next(proyecto_info.imagenes)}} >
+             <div className="w-max h-full flex items-center justify-center hover:text-zinc-400 cursor-pointer sm:px-3 px-1" onClick={()=>{ next(proyecto_info.imagenes)}} >
              <ArrowForwardIosIcon  style={{fontSize:36}}></ArrowForwardIosIcon>
              </div>
             
@@ -73,13 +73,25 @@ export default function Modal_info({setProyecto_info,proyecto_info}) {
                 {
                   proyecto_info.stack.map((s,i)=>{
                     return(
-                        <li className={`${stack[s].color} bg-opacity-20 p-2 px-8 flex items-center justify-center rounded-lg `} key={`a${i}`}>
+                        <li className={`${stack[s].color} bg-opacity-20 p-2 sm:px-8 px-4 flex items-center justify-center rounded-lg ${(stack[s].name=="TAILWIND") ? "py-3.5" : null} `} key={`a${i}`}>
                             <img className="w-8" src={`/Portafolios-sannti.dev/icons/${stack[s].img}`} alt="" />
                         </li>
                     )
                   })
                 }
 
+            </ul>
+
+            <ul className="flex flex-row gap-3 justify-between w-full px-3">
+              {
+                proyecto_info.link.map((url,i)=>{
+                  return(
+                    <li  key={`${i}b`} className="border border-zinc-600 rounded-lg text-zinc-400 hover:bg-zinc-800 cursor-pointer h-max py-2">
+                      <a className="w-full h-full p-5" target="_blank" href={url.link}>{url.name}</a>
+                    </li>
+                  )
+                })
+              }
             </ul>
             
           </div>
